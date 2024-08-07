2d3952eff1e958db93718244d2b62420cdea7773
Nguyen Ha <nguyeh3@uci.edu>
2024-08-06 21:55:44 -0700

    partially done homepage UI

---
+++ backend/Models/chatModel.js
@@ -0,0 +1,36 @@
+//chatName
+//isGroupChat
+//users
+//latesMessage
+//groupAdmin
+
+const mongoose = require('mongoose')
+
+const chatModel = mongoose.Schema(
+    {
+        chatName: { type: String, trim: true },
+        isGroupChat: { type: Boolean, defaul: false },
+        users: [
+            {
+                type: mongopose.Schema.Types.ObjectId,
+                ref: "User",
+            }
+        ],
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+        },
+        groupAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    }
+    {
+        timestamps: true,
+    }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.exports = Chat;
+
