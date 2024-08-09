03286e82da53a56f7e2b3b2e09e5ca727872b889
Nguyen Ha <nguyeh3@uci.edu>
2024-08-09 16:28:22 -0700

    connect db - move env to root

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { chats } = require("./data/data");
-const app = express();
-
 const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const colors = require("colors");
+
+const connectDB = require("./config/db");
 dotenv.config()
+connectDB();
+
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,4 +25,6 @@ app.get('/api/chat/:id', (req, res) => {
     res.send(singleChat);
 });
 
-app.listen(5000, console.log(`Server on PORT ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`.yellow.bold);
+});
